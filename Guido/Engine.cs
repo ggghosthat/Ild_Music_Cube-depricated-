6270db8359ce1e86d8fafaedcc7cabf36e39ad96
ggghosthat <ildarildar990@gmail.com>
2023-07-25 13:40:23 -0400

    engine refactored

--- Guido/Engine.cs
+++ Guido/Engine.cs
@@ -1,5 +1,7 @@
 using ShareInstances.Instances;
 using Cube.Mapper.Entities;
+
+using System.Linq;
 using System.Data.SQLite;
 using Dapper;
 namespace Cube.Storage;
@@ -46,156 +48,204 @@ public class Engine
 
     public void Add<T>(T entity)
     {
+        ReadOnlySpan<char> dapperQuery;
         if (entity is ArtistMap artist)
         {
+            dapperQuery = "insert or ignore into artists(AID, Name, Description, Avatar) values (@Buid, @Name, @Description, @Avatar)".AsSpan();
             using (var connection = new SQLiteConnection(_connectionString.ToString()))
             {                
-                connection.Execute("insert or ignore into artists(AID, Name, Description, Avatar) values (@Buid, @Name, @Description, @Avatar)", artist);     
+                connection.Execute(dapperQuery.ToString(), artist);     
             }
         }
         else if (entity is PlaylistMap playlist)
         {
+            dapperQuery = "insert or ignore into playlists(PID, Name, Description, Avatar) values (@Buid, @Name, @Description, @Avatar)".AsSpan();
             using (var connection = new SQLiteConnection(_connectionString.ToString()))
             {
-                connection.Execute("insert or ignore into playlists(PID, Name, Description, Avatar) values (@Buid, @Name, @Description, @Avatar)", playlist );
+                connection.Execute(dapperQuery.ToString(), playlist );
             }
         }
         else if (entity is TrackMap track)
         {
+            dapperQuery = "insert or ignore into tracks(TID, Path, Name, Description, Avatar, Valid, Duration) values (@Buid, @Pathway, @Name, @Description, @Avatar, @IsValid, @Duration)".AsSpan();
             using (var connection = new SQLiteConnection(_connectionString.ToString()))
             {
-                connection.Execute("insert or ignore into tracks(TID, Path, Name, Description, Avatar, Valid, Duration) values (@Buid, @Pathway, @Name, @Description, @Avatar, @IsValid, @Duration)", track);
+                connection.Execute(dapperQuery.ToString(), track);
             }
         }
         else if (entity is TagMap tag)
         {
+            dapperQuery = "insert or ignore into tags(TagID, Name, Color) values (@Buid, @Name, @Color)".AsSpan();
             using (var connection = new SQLiteConnection(_connectionString.ToString()))
             {
-                connection.Execute("insert or ignore into tags(TagID, Name, Color) values (@Buid, @Name, @Color)", tag);
+                connection.Execute(dapperQuery.ToString(), tag);
             }
         }
     }
 
     public void AddStores(ICollection<Store> stores)
     {
-        foreach(Store store in stores)
-        {
-            InsertStore(store);
-        }
+        stores.ToList().ForEach(store => 
+        {   
+            ReadOnlySpan<char> dapperQuery;
+            switch(store.Tag)
+            {
+                case(1):
+                    dapperQuery = "insert into artists_playlists(AID, PID) select @aid, @pid where not EXISTS(SELECT 1 from artists_playlists where AID = @aid and PID = @pid)".AsSpan();
+                    using (var connection = new SQLiteConnection(_connectionString.ToString()))
+                    {
+                        foreach(Guid relate in store.Relates)
+                        {
+                            connection.Execute(dapperQuery.ToString(), new {aid=store.Holder.ToString(), pid=relate.ToString() });
+                        }
+                    }
+                    break;
+                case(2):
+                    dapperQuery = "insert into artists_tracks(AID, TID) select @aid, @tid where not EXISTS(SELECT 1 from artists_tracks where AID = @aid and TID = @tid)".AsSpan();
+                    using (var connection = new SQLiteConnection(_connectionString.ToString()))
+                    {
+                        foreach(Guid relate in store.Relates)
+                        {
+                            connection.Execute(dapperQuery.ToString(), new {aid=store.Holder.ToString(), tid=relate.ToString() });
+                        }
+                    }
+                    break;
+                case(3):
+                    dapperQuery = "insert into artists_playlists(AID, PID) select @aid, @pid where not EXISTS(SELECT 1 from artists_playlists where AID = @aid and PID = @pid)".AsSpan();
+                    using (var connection = new SQLiteConnection(_connectionString.ToString()))
+                    {
+                        foreach(Guid relate in store.Relates)
+                        {
+                            connection.Execute(dapperQuery.ToString(), new {aid=relate.ToString(), pid=store.Holder.ToString() });
+                        }
+                    }
+                    break;
+                case(4):
+                    dapperQuery = "insert into playlists_tracks(PID, TID) select @pid, @tid where not EXISTS(SELECT 1 from playlists_tracks where PID = @pid and TID = @tid)".AsSpan();
+                    using (var connection = new SQLiteConnection(_connectionString.ToString()))
+                    {
+                        foreach(Guid relate in store.Relates)
+                        {
+                            connection.Execute(dapperQuery.ToString(), new {pid=store.Holder.ToString(), tid=relate.ToString() });
+                        }
+                    }
+                    break;
+                case(5):
+                    dapperQuery = "insert into artists_tracks(AID, TID) select @aid, @tid where not EXISTS(SELECT 1 from artists_tracks where AID = @aid and TID = @tid)";
+                    using (var connection = new SQLiteConnection(_connectionString.ToString()))
+                    {
+                        foreach(Guid relate in store.Relates)
+                        {
+                            connection.Execute(dapperQuery.ToString(), new {aid=relate.ToString(), tid=store.Holder.ToString() });
+                        }
+                    }
+                    break;
+                case(6):
+                    dapperQuery = "insert into playlists_tracks(PID, TID) select @pid, @tid where not EXISTS(SELECT 1 from playlists_tracks where PID = @pid and TID = @tid)";
+                    using (var connection = new SQLiteConnection(_connectionString.ToString()))
+                    {
+                        foreach(Guid relate in store.Relates)
+                        {
+                            connection.Execute(dapperQuery.ToString(), new {pid=relate.ToString(), tid=store.Holder.ToString() });
+                        }
+                    }
+                    break;
+                case(7):
+                    dapperQuery = "insert into tags_instances(TagID, IID) select @tagid, @iid where not EXISTS(SELECT 1 from tags_instances where TagID = @tagid and IID = @iid)";
+                    using (var connection = new SQLiteConnection(_connectionString.ToString()))
+                    {
+                        foreach(Guid relate in store.Relates)
+                        {
+                            connection.Execute(dapperQuery.ToString(), new {tagid=relate.ToString(), iid=store.Holder.ToString() });
+                        }
+                    }
+                    break;
+                default:break;
+            }
+        });
     }
 
-    private void InsertStore(Store store)
+    public void Edit<T>(T entity)
     {
-        switch(store.Tag)
+        ReadOnlySpan<char> dapperQuery;
+        if (entity is Artist artist)
         {
-            case(1):
-                using (var connection = new SQLiteConnection(_connectionString.ToString()))
+            dapperQuery = "".AsSpan();
+            using (var connection = new SQLiteConnection(_connectionString.ToString()))
+            {
+                foreach(Guid relate in store.Relates)
                 {
-                    foreach(Guid relate in store.Relates)
-                    {
-                        connection.Execute("insert into artists_playlists(AID, PID) select @aid, @pid where not EXISTS(SELECT 1 from artists_playlists where AID = @aid and PID = @pid)", new {aid=store.Holder.ToString(), pid=relate.ToString() });
-                    }
+                    connection.Execute(dapperQuery.ToString(), new {});
                 }
-                break;
-            case(2):
-                using (var connection = new SQLiteConnection(_connectionString.ToString()))
+            }
+        }
+        else if (entity is Playlist playlist)
+        {
+            dapperQuery = "".AsSpan();
+            using (var connection = new SQLiteConnection(_connectionString.ToString()))
+            {
+                foreach(Guid relate in store.Relates)
                 {
-                    foreach(Guid relate in store.Relates)
-                    {
-                        connection.Execute("insert into artists_tracks(AID, TID) select @aid, @tid where not EXISTS(SELECT 1 from artists_tracks where AID = @aid and TID = @tid)", new {aid=store.Holder.ToString(), tid=relate.ToString() });
-                    }
+                    connection.Execute(dapperQuery.ToString(), new {});
                 }
-                break;
-            case(3):
-                using (var connection = new SQLiteConnection(_connectionString.ToString()))
-                {
-                    foreach(Guid relate in store.Relates)
-                    {
-                        connection.Execute("insert into artists_playlists(AID, PID) select @aid, @pid where not EXISTS(SELECT 1 from artists_playlists where AID = @aid and PID = @pid)", new {aid=relate.ToString(), pid=store.Holder.ToString() });
-                    }
-                }
+            }
 
-                break;
-            case(4):
-                using (var connection = new SQLiteConnection(_connectionString.ToString()))
+        }
+        else if (entity is Track track)
+        {
+            dapperQuery = "".AsSpan();
+            using (var connection = new SQLiteConnection(_connectionString.ToString()))
+            {
+                foreach(Guid relate in store.Relates)
                 {
-                    foreach(Guid relate in store.Relates)
-                    {
-                        connection.Execute("insert into playlists_tracks(PID, TID) select @pid, @tid where not EXISTS(SELECT 1 from playlists_tracks where PID = @pid and TID = @tid)", new {pid=store.Holder.ToString(), tid=relate.ToString() });
-                    }
+                    connection.Execute(dapperQuery.ToString(), new {});
                 }
-                break;
-            case(5):
-                using (var connection = new SQLiteConnection(_connectionString.ToString()))
-                {
-                    foreach(Guid relate in store.Relates)
-                    {
-                        connection.Execute("insert into artists_tracks(AID, TID) select @aid, @tid where not EXISTS(SELECT 1 from artists_tracks where AID = @aid and TID = @tid)", new {aid=relate.ToString(), tid=store.Holder.ToString() });
-                    }
-                }
-                break;
-            case(6):
-                using (var connection = new SQLiteConnection(_connectionString.ToString()))
-                {
-                    foreach(Guid relate in store.Relates)
-                    {
-                        connection.Execute("insert into playlists_tracks(PID, TID) select @pid, @tid where not EXISTS(SELECT 1 from playlists_tracks where PID = @pid and TID = @tid)", new {pid=relate.ToString(), tid=store.Holder.ToString() });
-                    }
-                }
-                break;
-            case(7):
-                using (var connection = new SQLiteConnection(_connectionString.ToString()))
-                {
-                    foreach(Guid relate in store.Relates)
-                    {
-                        connection.Execute("insert into tags_instances(TagID, IID) select @tagid, @iid where not EXISTS(SELECT 1 from tags_instances where TagID = @tagid and IID = @iid)", new {tagid=relate.ToString(), iid=store.Holder.ToString() });
-                    }
-                }
-                break;
-            default:break;
+            }
+
         }
     }
 
-
+    public void EditStores(ICollection<Store> stores)
+    {
+        
+    }
 
     public void Delete<T>(ref T entity)
     {
+        ReadOnlySpan<char> dapperQuery;
         if(entity is Artist artist)
         {
-            string artistDeletionQuery = @"delete from artists where AID = @aid;
-                                          delete from artists_playlists where AID = @aid;
-                                          delete from artists_tracks where AID = @aid;";
+            dapperQuery = @"delete from artists where AID = @aid;
+                            delete from artists_playlists where AID = @aid;
+                            delete from artists_tracks where AID = @aid;".AsSpan();
 
             using (var connection = new SQLiteConnection(_connectionString.ToString()))
             {
-                connection.QueryMultiple(artistDeletionQuery, new {aid=artist.Id.ToString()});
+                connection.QueryMultiple(dapperQuery.ToString(), new {aid=artist.Id.ToString()});
             }
         }
         else if(entity is Playlist playlist)
         {
-            string playlistDeletionQuery = @"delete from playlists where PID = @pid;
-                                          delete from artists_playlists where PID = @pid;
-                                          delete from playlists_tracks where PID = @pid;";
+            dapperQuery = @"delete from playlists where PID = @pid;
+                            delete from artists_playlists where PID = @pid;
+                            delete from playlists_tracks where PID = @pid;".AsSpan();
 
             using (var connection = new SQLiteConnection(_connectionString.ToString()))
             {
-                connection.QueryMultiple(playlistDeletionQuery, new {pid=playlist.Id.ToString()});
+                connection.QueryMultiple(dapperQuery.ToString(), new {pid=playlist.Id.ToString()});
             }
         }
         else if(entity is Track track)
         {
-            string trackDeletionQuery = @"delete from tracks where TID = @tid;
-                                          delete from playlists_tracks where TID = @tid;
-                                          delete from artists_tracks where TID = @tid;";
+            dapperQuery = @"delete from tracks where TID = @tid;
+                            delete from playlists_tracks where TID = @tid;
+                            delete from artists_tracks where TID = @tid;".AsSpan();
 
             using (var connection = new SQLiteConnection(_connectionString.ToString()))
             {
-                connection.QueryMultiple(trackDeletionQuery, new {tid=track.Id.ToString()});
+                connection.QueryMultiple(dapperQuery.ToString(), new {tid=track.Id.ToString()});
             }
 
         }
     }
-
-    public void Edit<T>(T entity)
-    {}
 }
